5728e6a6364ed817d386e7ab80e9a3ede906a365
exerussus <solovyov.production@gmail.com>
2022-04-09 19:51:48 +0300

    Создана функция decypher.

--- function.py
+++ function.py
@@ -20,6 +20,21 @@ def title():
 
 
 def for_teacher():
+    """За кого замена"""
     teacher = 'моисеева'
     x = 'За ' + data_teachers[teacher]['родительный'] + ':'
     return x
+
+# def create_title_of_table():
+#
+
+
+def decypher(inputting_text):
+    x = inputting_text.split(', ')
+    date = x[0]
+    replacing_teacher = x[1][3:]
+    teacher = x[2]
+    index_class = x[3]
+    index_subject = x[4]
+    hours = x[5]
+    return date, replacing_teacher, teacher, index_class, index_subject, hours
--- test.py
+++ test.py
@@ -4,3 +4,5 @@ from function import *
 print(title())
 print(' ')
 print(for_teacher())
+x = decypher('31.01, за ярцеву, афонина, 6в, 2ур, 1ч')
+print(x)
